0cf68731ce194bac938ef34d92100390b4fece2b
jhjdl3984 <jhjdl3984@gmail.com>
2025-10-27 23:35:46 +0900

    Add files via upload

---
+++ db/04.PYMYSQL/test_pymysql.py
@@ -0,0 +1,72 @@
+import pymysql
+
+# (1) db connection
+connection = pymysql.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    db = 'classicmodels',
+    charset = 'utf8mb4',
+    cursorclass = pymysql.cursors.DictCursor
+)
+
+# (2) CRUD
+
+## 1. SELECT * FROM
+def get_customers():
+    cursor = connection.cursor()
+
+    sql = "SELECT * FROM customers"
+    cursor.execute(sql)
+
+    customers = cursor.fetchone()
+    print("customers : ", customers)
+    print("customers : ", customers['customerNumber'])
+    print("customers : ", customers['customerName'])
+    print("customers : ", customers['country'])
+    cursor.close()
+
+## 2. INSERT INTO
+def add_customer():
+    cursor = connection.cursor()
+
+    name = 'inseop'
+    family_name = 'kim'
+    sql = f"INSERT INTO customers(customerNumber, customerName, contactLastName) VALUES(1005, '{name}', '{family_name}')"
+    cursor.execute(sql)
+    connection.commit()
+    cursor.close()
+
+add_customer()
+
+## 3. UPDATE SET
+def update_customer():
+    cursor = connection.cursor()
+    update_name = 'update_inseop'
+    contactLastName = 'update_kim'
+    sql = f"UPDATE customers SET customerName = '{update_name}', contactLastName = '{contactLastName}' WHERE customerNumber = 1005"
+    cursor.execute(sql)
+    connection.commit()
+    cursor.close()
+
+update_customer()
+
+## 4. DELETE FROM
+def delete_customer():
+    cursor = connection.cursor()
+    sql = "DELETE FROM customers WHERE customerNumber = 1005"
+    cursor.execute(sql)
+    connection.commit()
+    cursor.close()
+
+delete_customer()
+
+
+
+
+
+
+
+
+
+
